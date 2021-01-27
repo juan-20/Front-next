6012a801e2621f58fdf2b84bcaedbd38b7d9a1f0
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-01-27 16:48:31 -0300

    Colocando metatags :bookmark:

--- pages/_app.js
+++ pages/_app.js
@@ -43,6 +43,23 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700" rel="stylesheet" />
+        {/* metatg: */}
+
+        <meta name="title" content="AluraQuiz" />
+        <meta name="description" content="Site mal feito não terminado de um quizz" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://quiz-star-wars-theta.vercel.app/" />
+        <meta property="og:title" content="AluraQuiz" />
+        <meta property="og:description" content="Site mal feito não terminado de um quizz" />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://quiz-star-wars-theta.vercel.app/" />
+        <meta property="twitter:title" content="AluraQuiz" />
+        <meta property="twitter:description" content="Site mal feito não terminado de um quizz" />
+        <meta property="twitter:image" content="" />
+
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
